fc52a0e5052e69970d354047303375b2e07ef8df
Gerhard Muntingh <gerhard@qux.nl>
2011-01-12 18:12:55 +0100

    Codify dependencies

--- Build.pl
+++ Build.pl
@@ -4,7 +4,9 @@ my $build = Module::Build->new
      module_name => 'FlatLdap',
      license  => 'perl',
      requires => {
-                  'perl'          => '5.10.0',
+                  'perl'              => '5.10.0',
+                  'Net::LDAP'         => '0.36',
+                  'Net::LDAP::Server' => '0.4',
                  },
     );
 $build->create_build_script;
